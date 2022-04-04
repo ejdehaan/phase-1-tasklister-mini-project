baf8f2a889d6c540b7f5f21eb345c5380135c1ca
Emily Joy DeHaan <emilydehaan001@gmail.com>
2022-04-03 20:12:27 -0400

    Updated
    https://github.com/ejdehaan/phase-1-tasklister-mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let newToDoForm = document.getElementById("create-task-form");
+  newToDoForm.addEventListener("submit", createNewTask);
 });
+
+function appendNewTask (toDo) {
+  document.getElementById("tasks").appendChild(toDo);
+};
+
+function createNewTask(event) {
+  event.preventDefault();
+  let newToDoDescr = document.getElementById("new-task-description");
+  let newToDo = document.createElement("li");
+  newToDo.innerText = newToDoDescr.value;
+  appendNewTask(newToDo);
+  event.target.reset();
+};
+
